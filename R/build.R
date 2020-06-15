9c1b7a211e26020dfacda721805c79ab377c3f14
Bill Behrman <behrman@stanford.edu>
2020-06-15 15:47:25 -0700

    Change file names to snake_case

--- R/build.R
+++ R/build.R
@@ -17,71 +17,72 @@ file_template_county <- here::here("templates/county.Rmd")
 file_template_state <- here::here("templates/state.Rmd")
   # About page template
 file_template_about <- here::here("templates/about.Rmd")
-  # Output directory 
+  # Output directory
 dir_output <- here::here("content")
+
 #===============================================================================
 
 options(blogdown.publishDir = here::here("docs"))
 
 # Counties
 counties <-
-  file_counties %>% 
-  read_csv() %>% 
-  pull(area) %>% 
+  file_counties %>%
+  read_csv() %>%
+  pull(area) %>%
   unique()
 
 # County files
 county_files <-
-  counties %>% 
-  str_remove(" County$") %>% 
-  str_to_lower() %>% 
-  str_replace_all(" ", "-") %>% 
-  fs::path(dir_output, ., ext = "Rmd") 
+  counties %>%
+  str_remove(" County$") %>%
+  str_to_lower() %>%
+  str_replace_all(" ", "_") %>%
+  fs::path(dir_output, ., ext = "Rmd")
 
 # Templates
 template_county <-
-  file_template_county %>% 
-  read_file() 
+  file_template_county %>%
+  read_file()
 
 template_state <-
-  file_template_state %>% 
+  file_template_state %>%
   read_file()
 
 template_about <-
-  file_template_about %>% 
+  file_template_about %>%
   read_file()
 
 create_page <- function(area, path, template) {
-  template %>% 
+  template %>%
     str_replace_all(
       c(
-        "params:\n  area: \"\"" = 
+        "params:\n  area: \"\"" =
           str_c("params: \n", str_glue("area: \"{area}\""), sep = "  "),
         "title: \"\"" = str_glue("title: \"{area}\"")
       )
-    ) %>% 
-    write_file(path) 
+    ) %>%
+    write_file(path)
 }
 
 walk2(
-  counties, 
-  county_files, 
+  counties,
+  county_files,
   create_page,
   template = template_county
 )
 
 create_page(
   area = yaml::read_yaml(file_params)$state,
-  path = 
+  path =
     fs::path(
-      dir_output, 
-      "state", 
+      dir_output,
+      "state",
       ext = "Rmd"
     ),
   template = template_state
 )
 
-template_about %>% 
-  read_file() %>% 
+template_about %>%
+  read_file() %>%
   write_file(fs::path(dir_output, "about", ext = "Rmd"))
 
