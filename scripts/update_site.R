ffc9a5eaf324aaaa7c3c735ec3f887b8d5f518e7
Bill Behrman <behrman@stanford.edu>
2020-06-16 15:56:44 -0700

    Initial commit

---
+++ scripts/update_site.R
@@ -0,0 +1,19 @@
+# Update site.
+
+# Author: Sara Altman, Bill Behrman
+# Version: 2020-06-16
+
+# Parameters
+  # Directory for site
+dir_site <- here::here("docs")
+
+#===============================================================================
+
+cli::cat_line(cli::rule(str_glue("Build site")))
+options(blogdown.publishDir = dir_site)
+blogdown::build_site(local = TRUE)
+
+cli::cat_line(cli::rule(str_glue("Push site to GitHub")))
+system("git add docs")
+system("git commit -m 'Update site'")
+system("git push")
